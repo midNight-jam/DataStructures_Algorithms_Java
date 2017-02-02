302ee26a7456cd09a6fe5d2a82fae6f9115dad11
midNight-jam <malviya.jayam@gmail.com>
2017-02-01 22:34:03 -0800

    lean radix

--- src/darkRealm/CTCI/Sorting_and_Searching/RadixSortUtil.java
+++ src/darkRealm/CTCI/Sorting_and_Searching/RadixSortUtil.java
@@ -8,21 +8,20 @@ public class RadixSortUtil {
   /* k is the most nuumber of digits in the array*/
   public static int[] radixSort(int[] arr, int k) {
     int powerTen = 10;
-    int belowPower = 1;
     for (int i = 0; i < k; i++) {
-      arr = modifiedCountingSort(arr, 10, powerTen, belowPower);
-      belowPower = powerTen;
+      // as we are sorting by digits, the range for digits is always 10 (0-9)
+      arr = modifiedCountingSort(arr, 10, powerTen);
       powerTen *= 10;
     }
     return arr;
   }
 
-  public static int[] modifiedCountingSort(int[] arr, int range, int powerTen, int belowPower) {
+  public static int[] modifiedCountingSort(int[] arr, int range, int powerTen) {
     int[] frequencyArr = new int[range];
     int digit = 0;
     for (int i = 0; i < arr.length; i++) {
       digit = arr[i] % powerTen;
-      digit = digit / belowPower;
+      digit = digit / (powerTen / 10);
       frequencyArr[digit]++;
     }
     for (int i = 1; i < range; i++) {
@@ -31,7 +30,7 @@ public class RadixSortUtil {
     int[] resultArr = new int[arr.length];
     for (int i = arr.length - 1; i >= 0; i--) {
       digit = arr[i] % powerTen;
-      digit = digit / belowPower;
+      digit = digit / (powerTen / 10);
       resultArr[frequencyArr[digit] - 1] = arr[i];
       frequencyArr[digit]--;
     }
