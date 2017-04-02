28e8de1a255175e4b5372570e84ca07ead745ad2
midNight-jam <malviya.jayam@gmail.com>
2017-04-01 17:14:31 -0700

    rotate

--- src/darkRealm/LongestSubStringWithOutRepeatingChars.java
+++ src/darkRealm/LongestSubStringWithOutRepeatingChars.java
@@ -1,7 +1,7 @@
 package darkRealm;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Jayam on 3/31/2017.
@@ -10,16 +10,21 @@ public class LongestSubStringWithOutRepeatingChars {
 
   public static int longestSubStringWithOutRepeatingChars(String str) {
     if (str == null || str.length() == 0) return 0;
-    Map<Character, Integer> map = new HashMap<>();
-    int maxLen = 0;
-    for (int start = 0, end = 0; end < str.length(); end++) {
-      if (map.containsKey(str.charAt(end))) {
-        start = Math.max(start, map.get(str.charAt(end)) + 1);
+    Set<Character> set = new HashSet<>();
+    int left = 0, right = 0, len = str.length(), max = Integer.MIN_VALUE;
+    char ch;
+    while (right < len) {
+      ch = str.charAt(right);
+      if (!set.contains(ch)) {
+        set.add(ch);
+        max = Math.max(set.size(), max);
+        right++;
+      } else {
+        set.remove(str.charAt(left));
+        left++;
       }
-      map.put(str.charAt(end), end);
-      maxLen = Math.max(maxLen, end - start + 1);
     }
-    return maxLen;
+    return max;
   }
 
   public static void main(String[] args) {
