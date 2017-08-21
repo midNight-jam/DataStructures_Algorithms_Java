4404c07cefb4215964a1bc98908e8a0cbbab8067
Jayam Malviya <malviya.jayam@gmail.com>
2017-08-20 17:42:28 -0700

    476: NUmber Complement

---
+++ src/darkRealm/NumberComplement.java
@@ -0,0 +1,24 @@
+package darkRealm;
+
+public class NumberComplement {
+/* Number Complement
+* Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation
+* Input: 5
+* Output: 2
+* Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+* */
+
+  public static int complementNumber(int n) {
+    int nbits = (int) (Math.log(n) / Math.log(2)) + 1;
+    int mask = 1 << nbits;
+    mask--;
+    return ~n & mask;
+  }
+
+  public static void main(String[] args) {
+//    int n = 5;
+    int n = 1;
+    int res = complementNumber(n);
+    System.out.println("number : " + n + "\nComplement : " + res);
+  }
+}
