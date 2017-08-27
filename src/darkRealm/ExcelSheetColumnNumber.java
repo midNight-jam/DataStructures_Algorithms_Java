a3e279302606189fcbcf1714a9b6c97cadd3612b
Jayam Malviya <malviya.jayam@gmail.com>
2017-08-26 22:47:55 -0700

    ExcelSheet Column Number

---
+++ src/darkRealm/ExcelSheetColumnNumber.java
@@ -0,0 +1,32 @@
+package darkRealm;
+
+public class ExcelSheetColumnNumber {
+
+//  Given a column title as appear in an Excel sheet, return its corresponding column number.
+//  For example:
+//  A -> 1
+//  B -> 2
+//  C -> 3
+//      ...
+//  Z -> 26
+//  AA -> 27
+//  AB -> 28
+
+  public static int titleToNumber(String s) {
+    int res = 0;
+    if (null == s) return res;
+    s = s.toUpperCase();
+    int pow26 = 1;
+    for (int i = s.length() - 1; i > -1; i--) {
+      res += pow26 * (s.charAt(i) - 64);
+      pow26 *= 26;
+    }
+    return res;
+  }
+
+  public static void main(String[] args) {
+    String s = "AAA";
+    int res = titleToNumber(s);
+    System.out.println("Str : " + s + "\nCol : " + res);
+  }
+}
