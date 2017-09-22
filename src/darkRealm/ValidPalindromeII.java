b46b1c14556bfcc5aaf2d07ccac2a41cff1f1ad7
Jayam Malviya <malviya.jayam@gmail.com>
2017-09-21 20:04:47 -0700

    ValidPalindromeII

---
+++ src/darkRealm/ValidPalindromeII.java
@@ -0,0 +1,39 @@
+package darkRealm;
+
+public class ValidPalindromeII {
+
+//  Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+//  Example 1:
+//  Input: "aba"
+//  Output: True
+//  Example 2:
+//  Input: "abca"
+//  Output: True
+//  Explanation: You could delete the character 'c'.
+
+  public static boolean validPalindrome(String s) {
+    int low = 0, high = s.length() - 1;
+    while (low <= high) {
+      if (s.charAt(low) != s.charAt(high))
+        return isPalindrome(s, low, high - 1) || isPalindrome(s, low + 1, high);
+      low++;
+      high--;
+    }
+    return true;
+  }
+
+  public static boolean isPalindrome(String s, int low, int high) {
+    for (; low <= high; low++, high--)
+      if (s.charAt(low) != s.charAt(high)) return false;
+    return true;
+  }
+
+  public static void main(String[] args) {
+//    String s = "aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga";
+    String s = "abc";
+//    String s = "lcuppucul";
+//    String s = "ebcbbececabbacecbbcbe";
+    boolean res = validPalindrome(s);
+    System.out.println("S : " + s + "\nR : " + res);
+  }
+}
