bb0c6a2a71b107dcf8a0f6d51139fbed7ce7ce11
Jayam Malviya <malviya.jayam@gmail.com>
2017-10-13 17:48:43 -0700

    PermutationsI

---
+++ src/darkRealm/Permutations.java
@@ -0,0 +1,61 @@
+package darkRealm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+
+
+
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        backtrack(res, temp, nums);
+        return res;
+    }
+
+    private static void backtrack(List<List<Integer>> resList, List<Integer> temp, int[] nums) {
+        if (temp.size() == nums.length) resList.add(new ArrayList<>(temp));
+        // there is no concept of start index like super set here we always begin from 0
+        for (int i = 0; i < nums.length; i++) {
+            if (temp.contains(nums[i])) continue; // as all the numbers are distinct, we are using its presence in temp list
+            // to determine that we have processed this element
+            temp.add(nums[i]);
+            backtrack(resList, temp, nums);
+            temp.remove(temp.size() - 1);
+        }
+    }
+
+    public static List<List<Integer>> permute_viaStr(int[] nums) {
+        List<Integer> numList = new ArrayList<>();
+        for (int i : nums)
+            numList.add(i);
+
+        List<List<Integer>> resList = new ArrayList<>();
+        combine(resList, numList, new ArrayList<>());
+        for (List<Integer> l : resList)
+            System.out.println(l);
+        return resList;
+    }
+
+    //Using the same logic as permutation of String ,
+    // slow ::: beat 2.8%
+    private static void combine(List<List<Integer>> res, List<Integer> numList, List<Integer> prefixList) {
+        if (numList.size() == 0) res.add(new ArrayList<>(prefixList));
+        for (int i = 0; i < numList.size(); i++) {
+            int picked = numList.remove(i);
+            prefixList.add(picked);
+            combine(res, numList, prefixList);
+            numList.add(i, picked);
+            prefixList.remove(prefixList.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+//        int[] nums = new int[]{1, 2, 3};
+        int[] nums = new int[]{1, 2, 1};
+        List<List<Integer>> res = permute(nums);
+        for (List<Integer> l : res)
+            System.out.println(l);
+    }
+}
