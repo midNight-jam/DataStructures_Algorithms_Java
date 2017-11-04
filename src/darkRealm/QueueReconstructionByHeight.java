7d494ae7dc2696828b87cc502a7936a49395b3aa
Jayam Malviya <malviya.jayam@gmail.com>
2017-11-04 12:25:40 -0700

    QueueReconstructionByHeight.java

--- src/darkRealm/QueueReconstructionByHeight.java
+++ src/darkRealm/QueueReconstructionByHeight.java
@@ -19,8 +19,7 @@ public class QueueReconstructionByHeight {
 
 
   public static int[][] reconstructQueue(int[][] people) {
-    if(people == null || people.length == 0 || people[0].length == 0) return people;
-
+    if (people == null || people.length == 0 || people[0].length == 0) return people;
     // Intuition is to sort people based on height in descending
     // and if their hieght matches then on their position in ascending
     // Why sort based on height in descending, because we utilize the linked list DS ability to push elements further
@@ -29,32 +28,41 @@ public class QueueReconstructionByHeight {
     Arrays.sort(people, new Comparator<int[]>() {
       @Override
       public int compare(int[] t1, int[] t2) {
-       // first sort based on height descending, then on position ascending
-        if(t1[0] == t2[0]) return t1[0] - t2[0];
+        // first sort based on height descending, then on position ascending
+        if (t1[0] == t2[0]) return t1[1] - t2[1];
         return t2[0] - t1[0];
       }
     });
 
     List<int[]> res = new LinkedList<>();
-    for(int [] p : people)
+    for (int[] p : people)
       res.add(p[1], p);
 
     int[][] finalRes = new int[res.size()][2];
-    for(int i = 0 ; i < res.size(); i++)
+    for (int i = 0; i < res.size(); i++)
       finalRes[i] = res.get(i);
-
     return finalRes;
   }
 
   public static void main(String[] args) {
+//    int[][] people = new int[][]{
+//        {5, 0}, {7, 0}, {5, 2}, {6, 1}, {4, 4}, {7, 1}
+//    };
+
     int[][] people = new int[][]{
-        {5, 0}, {7, 0}, {5, 2}, {6, 1}, {4, 4}, {7, 1}
+        {8, 2},
+        {4, 2},
+        {4, 5},
+        {2, 0},
+        {7, 2},
+        {1, 4},
+        {9, 1},
+        {3, 1},
+        {9, 0},
+        {1, 0}
     };
-
-
     int[][] res = reconstructQueue(people);
-    for (int[] p : res) {
+    for (int[] p : res)
       System.out.println(Arrays.toString(p));
-    }
   }
 }
