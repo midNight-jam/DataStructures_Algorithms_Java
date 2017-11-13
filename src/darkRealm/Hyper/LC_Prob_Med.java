9c8eb173d660cdfd9929c32c69c64f7567a74c49
Jayam Malviya <malviya.jayam@gmail.com>
2017-11-12 20:23:00 -0800

    BestTimeToBuyAndSellStock.java

--- src/darkRealm/Hyper/LC_Prob_Med.java
+++ src/darkRealm/Hyper/LC_Prob_Med.java
@@ -782,41 +782,6 @@ public class LC_Prob_Med {
   }
 
   /*  [Prob 121] Best Time to Buy and Sell Stock
-  * Say you have an array for which the ith element is the price of a given stock on day i.
-  * If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design
-  * an algorithm to find the maximum profit.
-  * Example 1:Input: [7, 1, 5, 3, 6, 4]
-  * Output: 5 max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)
-  * Example 2:
-  * Input: [7, 6, 4, 3, 1]
-  * Output: 0
-  * In this case, no transaction is done, i.e. max profit = 0.
-  * */
-  public static int maxProfit(int[] prices) {
-    if (prices == null || prices.length == 0) {
-      return 0;
-    }
-    int min, max;
-    min = max = prices[0];
-    int maxProfit = 0;
-    for (int i = 1; i < prices.length; i++) {
-      if (prices[i] < prices[i - 1] && prices[i] < min) {
-        if (max - min > maxProfit) {
-          maxProfit = max - min;
-        }
-        min = max = prices[i];
-      }
-      if (prices[i] > max) {
-        max = prices[i];
-      }
-    }
-    if (max - min > maxProfit) {
-      maxProfit = max - min;
-    }
-    return maxProfit;
-  }
-
-  /*  [Prob 121] Best Time to Buy and Sell Stock
     * Say you have an array for which the ith element is the price of a given stock on day i.
     *  You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times).
     *  However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
