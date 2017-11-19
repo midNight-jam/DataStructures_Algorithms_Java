a9bc416d694dc4e88fdeccae8924f7a2399d966b
Jayam Malviya <malviya.jayam@gmail.com>
2017-11-18 23:37:59 -0800

    GCD.java

---
+++ src/darkRealm/GCD.java
@@ -0,0 +1,24 @@
+package darkRealm;
+
+public class GCD {
+
+  private static int gcd(int big, int small) {
+    int temp;
+    // If we mod the smaller number from larger we reduce the larger number and GCD doesnt changes, so we keep
+    // moding till the remainder is 0
+    while (small > 0) {
+      temp = small;
+      small = big % small;
+      big = temp;
+    }
+    return big;
+  }
+
+  public static void main(String[] args) {
+//    int a = 10, b = 15;
+//    int a = 31, b = 2;
+    int a = 63, b = 28;
+    int gcd = gcd(b, a);
+    System.out.println("G : " + gcd);
+  }
+}
