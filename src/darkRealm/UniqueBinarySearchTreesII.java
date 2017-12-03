9699e64730a7f0e0ac1de042f521b78eb8d987b7
Jayam Malviya <malviya.jayam@gmail.com>
2017-12-03 15:41:46 -0800

    UniqueBinarySearchTreesII.java

--- src/darkRealm/UniqueBinarySearchTreesII.java
+++ src/darkRealm/UniqueBinarySearchTreesII.java
@@ -5,6 +5,18 @@ import java.util.List;
 
 public class UniqueBinarySearchTreesII {
 
+//  #95. Unique Binary Search Trees II
+//  Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1...n.
+//  For example,
+//  Given n = 3, your program should return all 5 unique BST's shown below.
+//
+//      1         3     3      2      1
+//      \       /     /      / \      \
+//      3     2     1      1   3      2
+//      /     /       \                 \
+//      2     1         2                 3
+
+
   static class TreeNode {
     int val;
     TreeNode left;
@@ -48,6 +60,6 @@ public class UniqueBinarySearchTreesII {
   }
 
   public static void main(String[] args) {
-    
+
   }
 }
