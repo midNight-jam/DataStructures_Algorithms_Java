d86b523fa01928568fb69f3e60ae1874c8473403
Jayam Malviya <malviya.jayam@gmail.com>
2017-12-03 16:57:34 -0800

    AddBinary.java

--- src/darkRealm/Hyper/LC_Prob3.java
+++ src/darkRealm/Hyper/LC_Prob3.java
@@ -31,29 +31,6 @@ public class LC_Prob3 {
     return max;
   }
 
-  public static String addBinary(String a, String b) {
-    int na = 0, nb = 0;
-    int i = a.length() - 1;
-    int j = b.length() - 1;
-    int sum = 0, carry = 0;
-    StringBuilder sb = new StringBuilder();
-    while (i >= 0 || j >= 0) {
-      sum = carry;
-      if (i >= 0) {
-        sum += a.charAt(i) - '0';
-        i--;
-      }
-      if (j >= 0) {
-        sum += b.charAt(j) - '0';
-        j--;
-      }
-      carry = sum / 2;
-      sum = sum % 2;
-      sb.append(sum);
-    }
-    if (carry == 1) sb.append('1');
-    return sb.reverse().toString();
-  }
 
   public static int addDigits(int num) {
     int sum = 0;
