4ffce9a08f5df29bb03147b0de7d99cb260b55ba
jayam.malviya <jayam.malviya@servicenow.com>
2017-12-05 16:43:51 -0800

    PascalsTriangleII.java

--- src/darkRealm/PascalsTriangleII.java
+++ src/darkRealm/PascalsTriangleII.java
@@ -7,8 +7,14 @@ import java.util.List;
 public class PascalsTriangleII {
 
 
-  /*  [Prob 119] ith Row of Pascals Triangle
- Pascal's Triangle II
+  /*  [Prob 119] 
+ Given an index k, return the kth row of the Pascal's triangle.
+
+For example, given k = 3,
+Return [1,3,3,1].
+
+Note:
+Could you optimize your algorithm to use only O(k) extra space?
 * */
   public static List<Integer> pascalsTriangleRow(int n) {
     Integer[] result =  new Integer[n + 1];
