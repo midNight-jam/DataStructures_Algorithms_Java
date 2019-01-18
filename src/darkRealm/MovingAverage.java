982a892e3b260b762e21068496a4c079817998d0
malviya.jayam@gmail.com <malviya.jayam@gmail.com>
2019-01-18 07:17:09 -0800

    MovingAverage.java

--- src/darkRealm/MovingAverage.java
+++ src/darkRealm/MovingAverage.java
@@ -1,7 +1,9 @@
 package darkRealm;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class MovingAverage {
 
@@ -14,30 +16,24 @@ public class MovingAverage {
 //    m.next(3) = (1 + 10 + 3) / 3
 //    m.next(5) = (10 + 3 + 5) / 3
 
-  private int windowSize;
-  private List<Integer> window;
-  private double movingAvg;
-  private long sum;
+  Queue<Integer> que;
+  int windowLength;
+  double sum;
 
   public MovingAverage(int size) {
-    windowSize = size;
-    this.sum = 0;
-    window = new ArrayList<>();
-    movingAvg = 0.0;
+    que = new LinkedList<>();
+    windowLength = size;
+    sum = 0;
   }
 
   public double next(int val) {
-    if (window.size() < windowSize) {
-      sum += val;
-      window.add(val);
-    } else {
-      sum -= window.get(0);
-      window.remove(0);
-      window.add(val);
-      sum += val;
-    }
-    movingAvg = sum / window.size();
-    return (int)movingAvg;
+    que.offer(val);
+    sum += val;
+
+    if(que.size() > windowLength)
+      sum -= que.poll();
+
+    return sum / que.size();
   }
 
   public static void main(String[] args) {
