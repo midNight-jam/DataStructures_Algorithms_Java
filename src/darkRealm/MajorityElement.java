22615649753253a7c03ec7ca92a67dc088f444eb
midNight-jam <malviya.jayam@gmail.com>
2019-03-03 23:48:34 -0800

    MajorityElement.java

--- src/darkRealm/MajorityElement.java
+++ src/darkRealm/MajorityElement.java
@@ -12,35 +12,19 @@ public class MajorityElement {
 
   public static int majorityElement(int[] nums) {
     if (null == nums || 0 == nums.length) return 0;
-    int count = 0;
-    int major = 0;
-    // we dont need the exact count of the major number as it is more than n/2, thus below works
-    for (int i = 0; i < nums.length; i++) {
-      if (count == 0) {
+    int major = nums[0];
+    int count = 1;
+    for (int i = 1; i < nums.length; i++) {
+      if (nums[i] == major)
         count++;
-        major = nums[i];
-      } else if (major == nums[i])
-        count++;
-      else
+      if (nums[i] != major)
         count--;
-    }
-    return major;
-  }
-
-  public static int majorityElementOLD(int[] nums) {
-    if (nums == null || 0 == nums.length) return 0;
-    Map<Integer, Integer> map = new HashMap<>();
-    int major = Integer.MIN_VALUE;
-    int res = 0;
-    for (int n : nums) {
-      map.put(n, map.getOrDefault(n, 0) + 1);
-      int currentMajor = Math.max(major, map.get(n));
-      if (currentMajor > major) {
-        major = currentMajor;
-        res = n;
+      if (count == 0) {
+        major = nums[i];
+        count = 1;
       }
     }
-    return res;
+    return major;
   }
 
   public static void main(String[] args) {
