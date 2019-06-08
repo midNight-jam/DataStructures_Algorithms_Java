cfbced42b69a48a24dd58f931201bfc441e23a26
Jayam Malviya <malviya.jayam@gmail.com>
2019-06-08 14:03:36 -0700

    Update FirstBadVersion.java

--- src/darkRealm/FirstBadVersion.java
+++ src/darkRealm/FirstBadVersion.java
@@ -11,22 +11,8 @@ public class FirstBadVersion {
 //  find the first bad version. You should minimize the number of calls to the API.
 
   
-   public int firstBadVersion(int n) {
-    int l = 0;
-    int r = n;
-    int mid;
-    while(l < r){
-      mid = l + (r - l) / 2;
-      if(!isBadVersion(mid) && !isBadVersion(mid + 1))
-        l = mid;
-      else if(isBadVersion(mid) && isBadVersion(mid + 1))
-        r = mid;
-      else return mid + 1;
-    }
-    return n;// this return doesnt matter, i hv submitted with both [l+1, n]
-  }
 
-  public int firstBadVersionOLD(int n) {
+  public int firstBadVersionFAST(int n) {
     int low = 1, high = n;
     int mid;
     while(low <= high){
@@ -40,4 +26,19 @@ public class FirstBadVersion {
     return low;
   }
 
+   // Calls API more # times
+   public int firstBadVersionSLOW(int n) {
+    int l = 0;
+    int r = n;
+    int mid;
+    while(l < r){
+      mid = l + (r - l) / 2;
+      if(!isBadVersion(mid) && !isBadVersion(mid + 1))
+        l = mid;
+      else if(isBadVersion(mid) && isBadVersion(mid + 1))
+        r = mid;
+      else return mid + 1;
+    }
+    return n;// this return doesnt matter, i hv submitted with both [l+1, n]
+  }
 }
