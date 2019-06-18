695b3145bf617de66145840d9fa63e98ae714ebe
Jayam Malviya <malviya.jayam@gmail.com>
2019-06-17 23:14:28 -0700

    Update SkyLineProblem.java

--- src/darkRealm/SkyLineProblem.java
+++ src/darkRealm/SkyLineProblem.java
@@ -46,6 +46,60 @@ public class SkyLineProblem {
     }
     return res;
   }
+  
+    public static List<List<Integer>> getSkylineEASYTOUNDERSTAND(int[][] buildings) {
+    Map<Integer, List<Integer>> leftEdge = new HashMap(); // map of list because at same point many buildings can start/end
+    Map<Integer, List<Integer>> rightEdge = new HashMap();
+    Set<Integer> points = new TreeSet<>(); // soreted set because these will be distinct points used for scanning our maps
+
+    for (int i = 0; i < buildings.length; i++) {
+      if(!leftEdge.containsKey(buildings[i][0]))
+        leftEdge.put(buildings[i][0], new ArrayList<>());
+      leftEdge.get(buildings[i][0]).add(buildings[i][2]);
+
+      if(!rightEdge.containsKey(buildings[i][1]))
+        rightEdge.put(buildings[i][1], new ArrayList<>());
+      rightEdge.get(buildings[i][1]).add(buildings[i][2]);
+
+      points.add(buildings[i][0]);
+      points.add(buildings[i][1]);
+    }
+
+    // maxHeap to keep the max height for any given point
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<>(new Comparator<Integer>() {
+      public int compare(Integer o1, Integer o2) {
+        if (o1 > o2) return -1;
+        else if (o2 > o1) return 1;
+        else return 0;
+      }
+    });
+
+    int height = 0;
+    int prevHeight = 0;
+    List<List<Integer>> res = new ArrayList<>();
+
+    for (int trav : points) {
+      if (leftEdge.containsKey(trav)){  // add all the heights that are starting at this point
+        List<Integer> heights = leftEdge.get(trav);
+        for(int  h : heights)
+          maxHeap.offer(h);
+      }
+
+      if (rightEdge.containsKey(trav)){// remove all the heights that are ending at this point
+        List<Integer> heights = rightEdge.get(trav);
+        for(int  h : heights)
+          maxHeap.remove(h);
+      }
+
+      height = maxHeap.size() > 0 ? maxHeap.peek() : 0;
+      if (height != prevHeight) {
+        res.add(new ArrayList<Integer>(Arrays.asList(trav, height)));
+      }
+      prevHeight = height;
+    }
+
+    return res;
+  }
 
   public static void main(String[] args) {
     int[][] buildings = new int[][]{{2, 9, 10}, {3, 7, 15}, {5, 12, 12}, {15, 20, 10}, {19, 24, 8}};
