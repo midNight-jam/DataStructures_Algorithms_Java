c28ff700b9dae7663cfea7165956f188d9b0585c
Jayam Malviya <malviya.jayam@gmail.com>
2019-06-23 18:03:20 -0700

    Update SearchInRotatedSortedArray.java

--- src/darkRealm/SearchInRotatedSortedArray.java
+++ src/darkRealm/SearchInRotatedSortedArray.java
@@ -35,20 +35,53 @@ public class SearchInRotatedSortedArray {
   }
 
   public static int search_findRotationAndSearchInTwoArrays(int[] nums, int target) {
-    if (nums == null || nums.length == 0) return -1;
-    int low = 0, high = nums.length - 1, mid = 0;
-    while (low < high) {
+    int low, high, mid;
+    low = 0;
+    high = arr.length - 1;
+    int res;
+    while(low <= high){
       mid = low + (high - low) / 2;
-      if (nums[mid] < nums[high]) high = mid;
-      else low = mid + 1;
+      
+      if(arr[low] <= arr[mid]){
+        // if target is within this range, perform usuals binarySearch
+        if(target >= arr[low] && target <= arr[mid]){
+          res = binarySearch(arr, low, mid, target);
+          return res < 0 ? -1 : res;
+        }
+        
+        else
+          low = mid + 1; // this half is sorted so skip this part
+      }
+      
+      else{
+        // if target is within this range, perform usuals binarySearch
+        if(target >= arr[mid] && target <= arr[high]){
+          res = binarySearch(arr, mid, high, target);
+          return res < 0 ? -1 : res;
+        }
+          
+        else 
+          high = mid - 1; // this half is sorted so skip this part
+      }
     }
-    int res = -1;
-    if (low > 0) {
-      res = Arrays.binarySearch(nums, 0, low, target);
-      res = res < 0 ? Arrays.binarySearch(nums, low, nums.length, target) : res;
-    } else res = Arrays.binarySearch(nums, 0, nums.length, target);
-
-    return res < 0 ? -1 : res;
+    
+    return -1;
+  }
+  
+  // normal binary Search
+  private int binarySearch(int [] arr, int low, int high, int target){
+    int mid;
+    while(low <= high){
+      mid = low + (high - low) / 2;
+      if(target == arr[mid])
+        return mid;
+      if(arr[mid] < target)
+        low = mid + 1;
+      else
+        high = mid - 1;
+    }
+    
+    return -1;
   }
 
   public static void main(String[] args) {
