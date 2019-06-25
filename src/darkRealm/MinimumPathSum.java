818ffbfe0d853d7e0a7880d20764f185f5e61b81
Jayam Malviya <malviya.jayam@gmail.com>
2019-06-24 23:27:54 -0700

    Update MinimumPathSum.java

--- src/darkRealm/MinimumPathSum.java
+++ src/darkRealm/MinimumPathSum.java
@@ -15,25 +15,35 @@ public class MinimumPathSum {
 //  Given the above grid map, return 7. Because the path 1→3→1→1→1 minimizes the sum.
 
   public static int minPathSum(int[][] grid) {
-    if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
-    int m = grid.length, n = grid[0].length;
-    int[][] dp = new int[m][n];
-    dp[0][0] = grid[0][0];
-    for (int i = 1; i < m; i++) // first col, only way to come in to first cols is by moving down thus add all the cost
-      dp[i][0] = dp[i - 1][0] + grid[i][0];
+   if(grid == null || grid.length < 1 || grid[0].length < 1) return 0;
+    int row = grid.length;
+    int col = grid[0].length;
+    int [][]dp = new int[row][col];
+    int top, left;
+    
+    // first row, only way to come in to first row is by moving right thus add all the cost
+    for(int i = 0, sum = 0; i < row; i++){
+      sum += grid[i][0];
+      dp[i][0] = sum;
+    }
 
-    for (int j = 1; j < n; j++) // first row, only way to come in to first row is by moving right thus add all the cost
-      dp[0][j] = dp[0][j - 1] + grid[0][j];
-
-
-    for (int i = 1; i < m; i++) {
-      for (int j = 1; j < n; j++) {
+    // first col, only way to come in to first cols is by moving down thus add all the cost
+    for(int j = 0, sum = 0; j < col; j++){
+      sum += grid[0][j];
+      dp[0][j] = sum;
+    }
+    
+    for(int i = 1; i < row; i++){
+      for(int j = 1; j < col; j++){
         // there are only two ways to enter this cell
         // either from top or from left, thus take min cost of it and add current cost
-        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+        top = dp[i-1][j];
+        left = dp[i][j-1];
+        dp[i][j] = Math.min(left, top) + grid[i][j];
       }
     }
-    return dp[m-1][n-1];
+    
+    return dp[row-1][col-1];
   }
 
   public static void main(String[] args) {
