12b05e8e72416480c422fb7fb05c19b45fc7990d
Jayam Malviya <malviya.jayam@gmail.com>
2019-06-27 23:10:09 -0700

    Update MissingRanges.java

--- src/darkRealm/MissingRanges.java
+++ src/darkRealm/MissingRanges.java
@@ -11,7 +11,52 @@ public class MissingRanges {
 //  missing ranges.
 //  For example, given [0, 1, 3, 50, 75], lower = 0 and upper = 99, return ["2", "4->49", "51->74", "76->99"].
 
-  public static List<String> findMissingRanges(int[] nums, int lower, int upper) {
+   public static List<String> findMissingRanges(int[] nums, int lower, int upper) {
+    List<String> res = new ArrayList<>();
+    String s;
+    if(nums == null) return res;
+    
+    // Empty array
+    if(nums.length < 1){
+      s = lower + "";
+      if(lower < upper)
+        s+= "->" + upper;
+      res.add(s);
+      return res;
+    }
+    
+    // lower is not in nums
+    if(lower < nums[0]){
+      s = (lower) + "";
+      if(lower + 1 < nums[0])
+        s+= "->" + (nums[0] - 1);
+      res.add(s);
+    }
+    
+    int head = nums[0];
+    for(int i = 1; i < nums.length; i++){
+      if(head == nums[i]) continue;
+      if(head + 1 < nums[i]){
+        s = (head + 1) + "";
+        if(head + 2 < nums[i])
+          s += "->" + (nums[i] - 1);
+        res.add(s);
+      }
+      head = nums[i];
+    }     
+    
+    // upper is not in nums
+    if(head < upper){
+        s = (head + 1) + "";
+        if(head + 2 <= upper)
+          s += "->" + (upper);
+        res.add(s);
+    }
+    
+    return res;
+  }
+  
+  public static List<String> findMissingRangesOLD(int[] nums, int lower, int upper) {
     int n = lower;
     String s;
     List<String> res = new ArrayList<>();
