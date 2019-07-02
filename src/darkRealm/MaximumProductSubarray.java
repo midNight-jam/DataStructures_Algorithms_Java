12e76202a0618c926fa7be4c4bc9270a3df3eea3
Jayam Malviya <malviya.jayam@gmail.com>
2019-07-01 23:08:47 -0700

    Update MaximumProductSubarray.java

--- src/darkRealm/MaximumProductSubarray.java
+++ src/darkRealm/MaximumProductSubarray.java
@@ -11,28 +11,33 @@ public class MaximumProductSubarray {
 
   public static int maxProduct(int[] nums) {
     if (nums == null || nums.length == 0) return 0;
-    if (nums == null || nums.length == 0) return 0;
+    
     // Intuition is to take two arrays for keeping the max and min Product till that element,
     // this product can be coming by multiplying of previous numbers or it can be a fresh stat(number itself)
     int[] minP = new int[nums.length];
     int[] maxP = new int[nums.length];
     minP[0] = maxP[0] = nums[0];
-    int max = nums[0];
+    int res = nums[0];
+    
     for (int i = 1; i < nums.length; i++) {
       // why we swap when we get a -ve number, bcoz the lowest min till now * -ve number will give us a +ve big number
       // that can be a maxProdct, thus swap!
       if (nums[i] < 0) {
+        //if -ve then swap, becuase -ve * -ve will be +ve & that +ve can be a candidate for res
         int t = maxP[i - 1];
         maxP[i - 1] = minP[i - 1];
         minP[i - 1] = t;
       }
-      minP[i] = Math.min(nums[i] * minP[i - 1], nums[i]);
-      maxP[i] = Math.max(nums[i] * maxP[i - 1], nums[i]);
-      max = Math.max(max, Math.max(maxP[i], minP[i]));
-
+      
+      // if producting with current num reduces the min product
+      minP[i] = Math.min(nums[i], nums[i] * minP[i-1]);
+      // if producting with current num increases the max product
+      maxP[i] = Math.max(nums[i], nums[i] * maxP[i-1]);
+      
+      res = Math.max(res, maxP[i]);
     }
-    // return which ever holds the max Product
-    return max;
+    
+    return res;
   }
 
   public static void main(String[] args) {
