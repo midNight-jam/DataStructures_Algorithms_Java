3faa200b5a8abef9c11dc15b96c91507e4276bd0
Jayam Malviya <malviya.jayam@gmail.com>
2019-07-18 22:56:02 -0700

    Update BestTimeToBuyAndSellStock.java

--- src/darkRealm/BestTimeToBuyAndSellStock.java
+++ src/darkRealm/BestTimeToBuyAndSellStock.java
@@ -19,6 +19,22 @@ public class BestTimeToBuyAndSellStock {
 
   public static int maxProfit(int[] prices) {
     if (prices == null || prices.length == 0) return 0;
+    int maxP = 0;
+    int min = prices[0], max = prices[0];
+    for (int i = 1; i < prices.length; i++) {
+      if (prices[i] < min) {
+        min = max = prices[i];
+      }
+      if (prices[i] > max && maxi >= mini) {
+        max = prices[i];
+        maxP = Math.max(maxP, max - min);
+      }
+    }
+    return maxP;
+  }
+  
+  public static int maxProfitOLD(int[] prices) {
+    if (prices == null || prices.length == 0) return 0;
     int maxP = 0, mini = 0, maxi = 0;
     int min = prices[0], max = prices[0];
     for (int i = 1; i < prices.length; i++) {
