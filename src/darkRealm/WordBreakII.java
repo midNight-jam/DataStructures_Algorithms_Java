d6eb022f452b03831659b626520972fc9e5297c3
midNight-jam <malviya.jayam@gmail.com>
2019-07-31 00:12:41 -0700

    WordBreakII.java

--- src/darkRealm/WordBreakII.java
+++ src/darkRealm/WordBreakII.java
@@ -21,22 +21,25 @@ public class WordBreakII {
   public static List<String> wordBreak(String str, List<String> dict) {
     if (str == null || str.length() == 0) return new ArrayList<>();
     Map<String, List<String>> map = new HashMap<>();
-    return wordBreakAllZZ(str, dict, map);
+    return helper(str, dict, map);
   }
 
-  private static List<String> wordBreakAllZZ(String str, List<String> dict, Map<String, List<String>> map) {
+  // break the string from behind for valid strings/suffix
+  private static List<String> helper(String str, List<String> dict, Map<String, List<String>> map) {
+    // if we have encountered this str/prefix before return the list against it
     if (map.containsKey(str)) return map.get(str);
-    String backPart;
+    String suffix;
     List<String> subList = new ArrayList<>();
     int len = str.length();
     if (dict.contains(str)) subList.add(str);
     for (int i = 1; i < len; i++) {
-      backPart = str.substring(i, len);
-      if (dict.contains(backPart)) {
-        String frontRem = str.substring(0, i);
-        List<String> remList = wordBreakAllZZ(frontRem, dict, map);
+      suffix = str.substring(i, len);
+      if (dict.contains(suffix)) {
+        String prefix = str.substring(0, i);
+        // get all the valid prefixes that can be formed using the dictionary & append this suffix to them
+        List<String> remList = helper(prefix, dict, map);
         for (String s : remList) {
-          subList.add(s + " " + backPart);
+          subList.add(s + " " + suffix);
         }
       }
     }
@@ -44,10 +47,52 @@ public class WordBreakII {
     return subList;
   }
 
+
+  public static List<String> wordBreakTLE(String s, List<String> wordDict) {
+    Set<String> dict = new HashSet<>(wordDict);
+    res = new ArrayList<>();
+    helperTLE(s, 0, dict, new ArrayList<>());
+    StringBuilder sbr = new StringBuilder();
+    List<String> fres = new ArrayList<>();
+    for (List<String> r : res) {
+      sbr.setLength(0);
+      for (String ss : r)
+        sbr.append(ss);
+      String f = sbr.toString().substring(0, sbr.length() - 1);
+      fres.add(f);
+    }
+    return fres;
+  }
+
+  static List<List<String>> res;
+
+  private static void helperTLE(String s, int index, Set<String> dict, List<String> list) {
+    if (index >= s.length()) {
+      res.add(new ArrayList<>(list));
+      return;
+    }
+
+    for (int i = index + 1; i <= s.length(); i++) {
+      String head = s.substring(index, i);
+      if (dict.contains(head)) {
+        list.add(head + " ");
+        helperTLE(s, i, dict, list);
+        list.remove(list.size() - 1);
+      }
+    }
+  }
+
+
   public static void main(String[] args) {
+//    String s = "catsanddog";
+//    List<String> dict = new ArrayList<>(Arrays.asList(new String[]{"cat", "cats", "and", "sand", "dog"}));
+//    String s = "pineapplepenapple";
+//    List<String> dict = new ArrayList<>(Arrays.asList(new String[]{"apple", "pen", "applepen", "pine", "pineapple"}));
+
     String s = "catsanddog";
-    List<String> dict = new ArrayList<>(Arrays.asList(new String[]{"cat", "cats", "and", "sand", "dog"}));
-    List<String> res  = wordBreak(s, dict);
+    List<String> dict = new ArrayList<>(Arrays.asList(new String[]{"cats", "dog", "sand", "and", "cat"}));
+
+    List<String> res = wordBreak(s, dict);
     for (String st : res)
       System.out.println(st);
   }
