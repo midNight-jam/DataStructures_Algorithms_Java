2bb6dc0b6f2fbe7d9c31e6635d5a9fde61508798
Jayam Malviya <malviya.jayam@gmail.com>
2019-08-10 21:58:08 -0700

    Update PalindromePermutationII.java

--- src/darkRealm/PalindromePermutationII.java
+++ src/darkRealm/PalindromePermutationII.java
@@ -22,70 +22,49 @@ public class PalindromePermutationII {
 //
 
   public static List<String> generatePalindromes(String s) {
+    List<String> res = new ArrayList<>();
+    boolean odd = false;
     Map<Character, Integer> map = new HashMap<>();
-    if (s == null || s.length() < 1)
-      return new ArrayList<String>();
-
-    int odd = 0;
-    int count;
-    char oddChar = ' ';
-
-    for (char c : s.toCharArray()) {
-      if (!map.containsKey(c))
+    for(char c : s.toCharArray()){
+      if(!map.containsKey(c))
         map.put(c, 0);
       map.put(c, map.get(c) + 1);
     }
-
-    for (char c : map.keySet()) {
-      count = map.get(c);
-      if ((count & 1) == 1) {
-        odd++;
-        oddChar = c;
+    
+    int len = 0;
+    char oc=' ';//oddchar
+    for(Character c : map.keySet()){
+      int f = map.get(c);
+      len += f;
+      if((f & 1) == 1){
+        oc = c;
+        if(!odd)
+          odd = true;
+        else// more than 1 odd
+          return new ArrayList<>();
       }
     }
-
-    if (odd > 1) // not possible to permute a palindrome
-      return new ArrayList<>();
-
-    String mid = "";
-    if (oddChar != ' ') {
-      mid += oddChar;
-      if (map.get(oddChar) == 1)
-        map.remove(oddChar);
-      else
-        map.put(oddChar, map.get(oddChar) - 1);
-    }
-    List<Character> palinChars = new ArrayList<>();
-    for (char c : map.keySet()) {
-      count = map.get(c) / 2; // add only half char as the same permutation will be used on the other side of the mid
-      while (count != 0) {
-        palinChars.add(c);
-        count--;
-      }
-    }
-
-    List<String> res = new ArrayList<>();
-    boolean[] used = new boolean[palinChars.size()];
-    helper(res, palinChars, new StringBuilder(), mid, used);
+    // reduce the odd to even
+    if(oc !=' ')
+      map.put(oc, map.get(oc) - 1);
+    
+    String start = oc == ' '? "" : ""+oc+""; // use the oddchar as start
+    helper(start, len, map, res);
     return res;
   }
 
-  private static void helper(List<String> res, List<Character> subPalins, StringBuilder palin, String mid, boolean[] used) {
-    if (palin.length() == subPalins.size()) {
-      StringBuilder sbr2 = new StringBuilder(palin);
-      // to create the palindrome add the other half reversed
-      res.add(palin.toString() + mid + sbr2.reverse().toString());
+   private static void helper(String p, int len, Map<Character, Integer> map,  List<String> res){
+    if(len == p.length()){
+      res.add(p);
       return;
     }
-
-    for (int i = 0; i < subPalins.size(); i++) {
-      // there can be duplicate chars even when we add half of them, so skip the same chars to generate unique permutations
-      if (used[i] || i > 0 && subPalins.get(i) == subPalins.get(i - 1) && used[i - 1]) continue;
-      used[i] = true;
-      palin.append(subPalins.get(i));
-      helper(res, subPalins, palin, mid, used);
-      palin.setLength(palin.length() - 1);
-      used[i] = false;
+    
+    for(Character c : map.keySet()){
+      if(map.get(c) == 0) continue;
+      String next = c + p + c;
+      map.put(c, map.get(c) - 2);
+      helper(next, len, map, res);
+      map.put(c, map.get(c) + 2);
     }
   }
 
