bcc52ebbb7d9cbb3416418f6dbdbbe428e327811
Jayam Malviya <malviya.jayam@gmail.com>
2019-08-25 20:08:21 -0700

    Update BinarySearch.java

--- src/darkRealm/BinarySearch.java
+++ src/darkRealm/BinarySearch.java
@@ -2,6 +2,7 @@ package darkRealm;
 
 public class BinarySearch {
 
+//   704. Binary Search
 //  Given a sorted (in ascending order) integer array nums of n elements and a target value, write a function to search target in nums. If target exists, then return its index, otherwise return -1.
 //
 //  Example 1:
