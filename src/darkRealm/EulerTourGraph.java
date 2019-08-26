547e8b167a19956a2603ca24de31a24fdcf94be2
midNight-jam <malviya.jayam@gmail.com>
2019-08-25 20:11:54 -0700

    EulerTourGraph.java

--- src/darkRealm/EulerTourGraph.java
+++ src/darkRealm/EulerTourGraph.java
@@ -4,6 +4,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/*
+ * In graph theory, an Eulerian trail (or Eulerian path) is a trail in a finite graph that visits every edge exactly
+ * once (allowing for revisiting vertices). Similarly, an Eulerian circuit or Eulerian cycle is an Eulerian trail that
+ * starts and ends on the same vertex. They were first discussed by Leonhard Euler while solving the famous Seven
+ * Bridges of Königsberg problem in 1736. The problem can be stated mathematically like this:
+ * Given the graph in the image, is it possible to construct a path (or a cycle, i.e. a path starting and ending on
+ * the same vertex) that visits each edge exactly once?
+ * */
+
 public class EulerTourGraph {
 
   List<List<Integer>> adjList;
