9341b7f960799c8e0a39e6af9589cf8495aba753
midNight-jam <malviya.jayam@gmail.com>
2019-08-29 22:54:24 -0700

    BinaryTreeColoringGame.java

--- src/darkRealm/BinaryTreeColoringGame.java
+++ src/darkRealm/BinaryTreeColoringGame.java
@@ -1,4 +1,98 @@
 package darkRealm;
 
 public class BinaryTreeColoringGame {
+
+//  1145. Binary Tree Coloring Game
+//  Two players play a turn based game on a binary tree.  We are given the root of this binary tree, and the number of
+//  nodes n in the tree.  n is odd, and each node has a distinct value from 1 to n.
+//
+//  Initially, the first player names a value x with 1 <= x <= n, and the second player names a value y with
+//  1 <= y <= n and y != x.  The first player colors the node with value x red, and the second player colors the node
+//  with value y blue.
+//
+//  Then, the players take turns starting with the first player.  In each turn, that player chooses a node of their
+//  color (red if player 1, blue if player 2) and colors an uncolored neighbor of the chosen node (either the left
+//  child, right child, or parent of the chosen node.)
+//
+//  If (and only if) a player cannot choose such a node in this way, they must pass their turn.  If both players pass
+//  their turn, the game ends, and the winner is the player that colored more nodes.
+//
+//  You are the second player.  If it is possible to choose such a y to ensure you win the game, return true.
+//  If it is not possible, return false.
+//
+//
+//  Example 1:
+//
+//
+//  Input: root = [1,2,3,4,5,6,7,8,9,10,11], n = 11, x = 3
+//  Output: true
+//  Explanation: The second player can choose the node with value 2.
+//
+//
+//  Constraints:
+//
+//  root is the root of a binary tree with n nodes and distinct node values from 1 to n.
+//  n is odd.
+//  1 <= x <= n <= 100
+
+  static class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+
+  /*
+   * Dont fall in the trap pf taking turns & coloring the nodes & finally at the end conculding win or loss, it gets
+   * complicated very easily.
+   * Intead,  we hv 2 helps already in the question
+   * 1: N is odd
+   * 2: x, the node that opponent is choosing is given
+   * If we draw the tree on the paper as per the constraints of the question we can easily see that to make opponnent
+   * loose we hv to limit the nodes he can color, to restrict him to the fewest nodes the can color from node x there are
+   * only 3 ways, left child, right child or the parent. The crux is we try to find the size which we will get to color
+   * from x (we color then he cant color & is restricted).
+   * Thus, if at x, we try to get the size of left & right sub tress & as the total is given we can also find what it size
+   * of rest if the tree. Now we hv to pick just the largest one & opponent cannot use any node from it & we win.
+   * Else the opponent always wins!
+   * */
+
+  public static boolean btreeGameWinningMove(TreeNode root, int n, int x) {
+    if (root == null) return false;
+    if (root.val == x) {
+      int left = size(root.left);
+      int right = size(root.right);
+      int par = n - (left + right + 1);
+
+      boolean win = par > (left + right) || left > (par + right) || right > (par + left);
+      if (win) return true;
+    }
+
+    return btreeGameWinningMove(root.left, n, x) || btreeGameWinningMove(root.right, n, x);
+  }
+
+  public static int size(TreeNode root) {
+    if (root == null) return 0;
+    return size(root.left) + size(root.right) + 1;
+  }
+
+  public static void main(String[] args) {
+    TreeNode root = new TreeNode(1);
+    root.left = new TreeNode(2);
+    root.right = new TreeNode(3);
+    root.left.left = new TreeNode(4);
+    root.left.right = new TreeNode(5);
+    root.right.left = new TreeNode(6);
+    root.right.right = new TreeNode(7);
+    root.left.left.left = new TreeNode(8);
+    root.left.left.right = new TreeNode(9);
+    root.left.right.left = new TreeNode(10);
+    root.left.right.right = new TreeNode(11);
+
+    boolean res = btreeGameWinningMove(root, 11, 3);
+    System.out.println(res);
+  }
 }
--- src/darkRealm/SnapShotArray.java
+++ src/darkRealm/SnapShotArray.java
@@ -1,4 +1,93 @@
 package darkRealm;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
 public class SnapShotArray {
+
+//  1146. Snapshot Array
+//  Implement a SnapshotArray that supports the following interface:
+//
+//  SnapshotArray(int length) initializes an array-like data structure with the given length.
+//  Initially, each element equals 0.
+//  void set(index, val) sets the element at the given index to be equal to val.
+//  int snap() takes a snapshot of the array and returns the snap_id: the total number of times we called snap() minus 1.
+//  int get(index, snap_id) returns the value at the given index, at the time we took the snapshot with the given snap_id
+//
+//
+//  Example 1:
+//
+//  Input: ["SnapshotArray","set","snap","set","get"]
+//      [[3],[0,5],[],[0,6],[0,0]]
+//  Output: [null,null,0,null,5]
+//  Explanation:
+//  SnapshotArray snapshotArr = new SnapshotArray(3); // set the length to be 3
+//  snapshotArr.set(0,5);  // Set array[0] = 5
+//  snapshotArr.snap();  // Take a snapshot, return snap_id = 0
+//  snapshotArr.set(0,6);
+//  snapshotArr.get(0,0);  // Get the value of array[0] with snap_id = 0, return 5
+//
+//
+//  Constraints:
+//
+//  1 <= length <= 50000
+//  At most 50000 calls will be made to set, snap, and get.
+//  0 <= index < length
+//  0 <= snap_id < (the total number of times we call snap())
+//  0 <= val <= 10^9
+
+  static class SnapshotArray {
+    int snapId;
+    List<TreeMap<Integer, Integer>> list;
+
+    public SnapshotArray(int len) {
+      snapId = 0;
+      list = new ArrayList<>();
+      for (int i = 0; i < len; i++) {
+        list.add(new TreeMap<>());
+        list.get(i).put(0, 0); // because for all indexes default value is 0 for snapId 0
+      }
+    }
+
+    public void set(int index, int val) {
+      list.get(index).put(snapId, val);
+    }
+
+    public int snap() {
+      return snapId++;
+    }
+
+    public int get(int index, int snap_id) {
+      TreeMap<Integer, Integer> map = list.get(index);
+      return map.get(map.floorKey(snap_id));
+    }
+  }
+
+  public static void main(String[] args) {
+    SnapshotArray sarr = new SnapshotArray(3);
+    sarr.set(0, 5);
+    sarr.set(1, 33);
+    int snapId = sarr.snap();
+    System.out.println("snapId : " + snapId);
+    sarr.set(0, 6);
+    snapId = sarr.snap();
+    System.out.println("snapId : " + snapId);
+    for (int i = 0; i < 3; i++)
+      System.out.print(sarr.get(0, i) + "  ");
+    System.out.println();
+    for (int i = 0; i < 3; i++)
+      System.out.print(sarr.get(1, i) + "  ");
+    System.out.println();
+    sarr.set(0, 7);
+    sarr.set(0, 8);
+    snapId = sarr.snap();
+    System.out.println("snapId : " + snapId);
+    for (int i = 0; i < 13; i++)
+      System.out.print(sarr.get(0, i) + "  ");
+    System.out.println();
+    for (int i = 0; i < 3; i++)
+      System.out.print(sarr.get(1, i) + "  ");
+    System.out.println();
+  }
 }
