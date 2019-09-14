cdcae3fbc1b79ee739bca1cb7abb36d49b62b6ef
Jayam Malviya <malviya.jayam@gmail.com>
2019-09-14 16:48:17 -0700

    Update BinaryIndexedTree.java

--- src/ADT/BinaryIndexedTree.java
+++ src/ADT/BinaryIndexedTree.java
@@ -3,51 +3,54 @@ package ADT;
 public class BinaryIndexedTree {
 
   int[] arr;
-  int[] bit;
+  int[] bitTree;
   int N;
 
-  BinaryIndexedTree(int[] arr) {
-    if (arr == null || arr.length < 1) return;
-    this.arr = arr;
-    bit = new int[arr.length + 1]; // N + 1, bcoz we ignore bit[0] : think of it as a root place holder but no value
+  BinaryIndexedTree(int[] nums) {
+    if(nums == null || nums.length < 1) return;
+    arr = new int[nums.length];
     N = arr.length;
-    constructBinaryIndexedTree();
+    bitTree = new int[nums.length + 1];// N + 1, bcoz we ignore bit[0] : think of it as a root place holder but no value
+    for(int i = 0; i < arr.length; i++){
+      updateBit(i, nums[i]);
+      arr[i] = nums[i];
+    }
   }
 
   // Time COmplexity O(NlogN)
-  private void constructBinaryIndexedTree() {
-    for (int i = 1; i <= N; i++)
-      update(i, arr[i - 1]);
-  }
-
-  // TimeComplexity O(LogN) as we loop only as many times as the 1 bits in the index
-  public void update(int index, int val) {
+   private void updateBit(int index, int val){
     // Get the last set bit in an integer
     // Example: x = 10(in decimal) = 1010(in binary)
     // The last set bit is given by x&(-x) = (10)1(0) & (01)1(0) = 0010 = 2 (in decimal)
 
-    while (index <= N) {
-      bit[index] += val;
-      int minusIndex = -index;
-      String bi = Integer.toBinaryString(index);
-      String mbi = Integer.toBinaryString(minusIndex);
-      index += (index & minusIndex);
+    index += 1;
+    while(index <= arr.length){
+      bitTree[index] += val;
+      index += index & (-index);
     }
+  }
+
+   public void update(int i, int val) {
+    int diff = val - arr[i];
+    arr[i] = val;
+    updateBit(i, diff);
   }
 
   // TimeComplexity O(LogN) as we loop only as many times as the 1 bits in the index
-  public int query(int index) {
+  private int query(int index){
+     index += 1;
     int sum = 0;
-    while (index > 0) {
-      sum += bit[index];
-      index -= (index & -index);
+    while(index > 0){
+      sum += bitTree[index];
+      index -= index & (-index);
     }
+    
     return sum;
   }
 
-  public int queryRange(int start, int end) {
-    //exclude any sum before start
-    return query(end) - query(start - 1);
+ public int sumRange(int i, int j) {
+   //exclude any sum before start
+    return query(j) - query(i - 1);
   }
 
   public static void main(String[] args) {
@@ -60,7 +63,7 @@ public class BinaryIndexedTree {
     int res;
     for (int re = nums.length; re > 0; re--) {
       for (int le = 1; le <= re; le++) {
-        res = bit.queryRange(le, re);
+        res = bit.sumRange(le, re);
         System.out.println((le) + ":" + (re) + " = " + res);
       }
     }
