71a8a17385a1ac4677fe3f96060c4c280f4389b8
Jayam Malviya <malviya.jayam@gmail.com>
2019-09-14 17:16:14 -0700

    Update SegmentTree.java

--- src/ADT/SegmentTree.java
+++ src/ADT/SegmentTree.java
@@ -9,88 +9,91 @@ public class SegmentTree {
   int[] seg_tree;
   int N;
 
-  SegmentTree(int[] arr) {
-    if(arr == null || arr.length < 1) return;
-    this.arr = arr;
-    N = arr.length;
+  SegmentTree(int[] nums) {
+      if (nums == null || nums.length < 1) return;
 
-    // Height of a full binary tree which can accomodate N nodes is LOG_base2_N + 1
-    int height = (int) Math.floor((Math.log(N) / Math.log(2))) + 1;
+      this.arr = nums;
+      N = arr.length;
 
-    // Max no of nodes in full Binary tree are (2^(height+1) - 1)
-    int no_of_nodes = (int) Math.pow(2, height + 1) - 1;
-    seg_tree = new int[no_of_nodes];
+      // Height of a full binary tree which can accomodate N nodes is LOG_base2_N
+      int height = (int) Math.ceil((Math.log(N) / Math.log(2)));
 
-    // kick off the construction of seg tree
-    build(0, 0, N - 1);
-  }
+      // Max no of nodes in full Binary tree are (2^(height+1) - 1)
+      int no_of_nodes = (int) Math.pow(2, height + 1) - 1;
+      segTree = new int[no_of_nodes];
 
-  private void build(int node, int start, int end) {
-    if (start == end) {
-      seg_tree[node] = arr[start];  // its a leaf node, take the value from input arr
-      return;
-    }
-    int mid = start + (end - start) / 2;
-
-    // as seg tree is a binary tree
-    int leftChild = 2 * node + 1;
-    int rightChild = 2 * node + 2;
-    // recurse for left child
-    build(leftChild, start, mid);
-    // recurse for right child
-    build(rightChild, mid + 1, end);
-
-    seg_tree[node] = seg_tree[leftChild] + seg_tree[rightChild];
-  }
-
-  public void update(int index, int val) {
-    int diff = val - arr[index];
-    updateHelper(0, 0, N - 1, index, diff);
-  }
-
-  // Complexity of update O(logN)
-  private void updateHelper(int node, int start, int end, int index, int diff) {
-    if (start == end) {
-      arr[index] += diff;
-      seg_tree[node] += diff;
-      return;
+      // kick off the construction of seg tree
+      build(0, 0, N - 1);
     }
 
-    int mid = start + (end - start) / 2;
+    private void build(int node, int start, int end) {
+      if (start == end) {
+        segTree[node] = arr[start];  // its a leaf node, take the value from input arr
+        return;
+      }
+      int leftChild = 2 * node + 1;
+      int rightChild = 2 * node + 2;
+      int mid = start + (end - start) / 2;
+      build(leftChild, start, mid);
+      build(rightChild, mid + 1, end);
+      segTree[node] = segTree[leftChild] + segTree[rightChild];
+    }
 
-    // if updating value in left subtree
-    if (start <= index && index <= mid)
-      updateHelper((2 * node) + 1, start, mid, index, diff);
+    public void update(int index, int val) {
+      if (index < 0 || index >= arr.length) return;
+      int diff = val - arr[index];
+      updateSegTree(0, 0, N - 1, index, diff);
+    }
 
-      // if updating value in right subtree
-    else
-      updateHelper((2 * node) + 2, mid + 1, end, index, diff);
+    // Complexity of update O(logN)
+    private void updateSegTree(int node, int start, int end, int index, int diff) {
+      if (start == end) {
+        arr[index] += diff;
+        segTree[node] += diff;
+        return;
+      }
 
-    // update the segtree while returning from recursion
-    seg_tree[node] = seg_tree[2 * node + 1] + seg_tree[2 * node + 2];
-  }
+      int mid = start + (end - start) / 2;
+      int leftChild = 2 * node + 1;
+      int rightChild = 2 * node + 2;
 
-  public int query(int left, int right) {
-    return queryHelper(0, 0, N - 1, left, right);
-  }
+      // if updating value in left subtree
+      if (index <= mid)
+        updateSegTree(leftChild, start, mid, index, diff);
 
-  // Complexity of query O(logN)
-  private int queryHelper(int node, int start, int end, int left, int right) {
-    // query out of this range
-    if (left > end || right < start)
-      return 0;
+        // if updating value in right subtree
+      else
+        updateSegTree(rightChild, mid + 1, end, index, diff);
 
-    // this range is within query,  so it will contribute to the result
-    if (left <= start && end <= right)
-      return seg_tree[node];
+      // update the segtree while returning from recursion
+      segTree[node] = segTree[leftChild] + segTree[rightChild];
+    }
 
-    // query partially within range, get the partial results from left & right child, sum them & return
-    int mid = start + (end - start) / 2;
+    public int sumRange(int left, int right) {
+      if (right < left) return 0;
+      return queryHelper(0, 0, N - 1, left, right);
+    }
 
-    int subRes1 = queryHelper(2 * node + 1, start, mid, left, right);
-    int subRes2 = queryHelper(2 * node + 2, mid + 1, end, left, right);
-    return subRes1 + subRes2;
-  }
+
+    // Complexity of query O(logN)
+    private int queryHelper(int node, int start, int end, int left, int right) {
+      // query out of this range
+      if (left > end || right < start)
+        return 0;
+
+      // this range is within query,  so it will contribute to the result
+      if (left <= start && end <= right)
+        return segTree[node];
+
+      // query partially within range, get the partial results from left & right child, sum them & return
+      int mid = start + (end - start) / 2;
+      int leftChild = 2 * node + 1;
+      int rightChild = 2 * node + 2;
+
+      int subRes1 = queryHelper(leftChild, start, mid, left, right);
+      int subRes2 = queryHelper(rightChild, mid + 1, end, left, right);
+      return subRes1 + subRes2;
+    }
 
   public static void main(String[] args) {
 //    int[] arr = new int[]{1, 3, 5};
