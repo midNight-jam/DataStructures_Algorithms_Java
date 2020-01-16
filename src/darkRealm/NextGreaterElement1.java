797cf1830fa64add8154e40a397c3c971ba63c6c
midNight-jam <malviya.jayam@gmail.com>
2020-01-15 21:33:41 -0800

    changes

--- src/darkRealm/NextGreaterElement1.java
+++ src/darkRealm/NextGreaterElement1.java
@@ -3,9 +3,11 @@ package darkRealm;
 import java.util.*;
 
 public class NextGreaterElement1 {
-//  You are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2. Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
+//  You are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2.
+//  Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
 //
-//  The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2. If it does not exist, output -1 for this number.
+//  The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2. If it does not
+//  exist, output -1 for this number.
 //
 //      Example 1:
 //  Input: nums1 = [4,1,2], nums2 = [1,3,4,2].
@@ -15,7 +17,32 @@ public class NextGreaterElement1 {
 //  For number 1 in the first array, the next greater number for it in the second array is 3.
 //  For number 2 in the first array, there is no next greater number for it in the second array, so output -1.
 
+
   public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+    if (nums1 == null || nums2 == null) return new int[0];
+    Map<Integer, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < nums1.length; i++)
+      map.put(nums1[i], -1);
+
+    Stack<Integer> stack = new Stack<>();
+
+    for (int i = nums2.length - 1; i > -1; i--) {
+      while (!stack.isEmpty() && nums2[i] >= stack.peek())
+        stack.pop();
+      if (map.containsKey(nums2[i]))
+        map.put(nums2[i], stack.isEmpty() ? -1 : stack.peek());
+      stack.push(nums2[i]);
+    }
+
+    int[] res = new int[nums1.length];
+    for (int i = 0; i < nums1.length; i++)
+      res[i] = map.get(nums1[i]);
+
+    return res;
+  }
+
+  public static int[] nextGreaterElementBASIC(int[] nums1, int[] nums2) {
     if (nums1 == null || nums2 == null || nums1.length > nums2.length)
       return new int[0];
     Map<Integer, Integer> map = new HashMap<>();
