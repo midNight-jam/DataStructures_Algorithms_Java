622d6f3733d78d3faf9dfd59b6349f410aa5d8db
Jayam Malviya <malviya.jayam@gmail.com>
2020-01-16 23:19:30 -0800

    Update TwoSum4BST.java

--- src/darkRealm/TwoSum4BST.java
+++ src/darkRealm/TwoSum4BST.java
@@ -3,7 +3,7 @@ package darkRealm;
 import java.util.*;
 
 public class TwoSum4BST {
-
+//  653. Two Sum IV - Input is a BST
 //  Given a Binary Search Tree and a target number, return true if there exist two elements in the BST such
 //  that their sum is equal to the given target.
 //
