2958f1afd572e2974c5fe359fdae542d37f4c4a1
midNight-jam <malviya.jayam@gmail.com>
2020-01-16 23:48:51 -0800

    MergeTwoBinaryTrees.java

--- src/darkRealm/MergeTwoBinaryTrees.java
+++ src/darkRealm/MergeTwoBinaryTrees.java
@@ -2,25 +2,28 @@ package darkRealm;
 
 public class MergeTwoBinaryTrees {
 
-//  Given two binary trees and imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not.
+//  617. Merge Two Binary Trees
+//  Given two binary trees and imagine that when you put one of them to cover the other, some nodes of the two trees
+//  are overlapped while the others are not.
 //
-//  You need to merge them into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of new tree.
+//  You need to merge them into a new binary tree. The merge rule is that if two nodes overlap, then sum node values
+//  up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of new tree.
 //
 //      Example 1:
 //  Input:
 //  Tree 1                     Tree 2
-//      1                         2
+//       1                         2
 //      / \                       / \
-//      3   2                     1   3
-//      /                           \   \
-//      5                             4   7
+//     3   2                     1   3
+//    /                           \   \
+//   5                             4   7
 //  Output:
 //  Merged tree:
 //      3
-//      / \
-//      4   5
-//      / \   \
-//      5   4   7
+//     / \
+//    4   5
+//   / \   \
+//  5   4   7
 
 
   public static class TreeNode {
