2efe936e498f3350b1e407383756416603a86790
Jayam Malviya <malviya.jayam@gmail.com>
2020-01-19 13:25:14 -0800

    Update BST_To_GreaterTree.java

--- src/darkRealm/BST_To_GreaterTree.java
+++ src/darkRealm/BST_To_GreaterTree.java
@@ -2,7 +2,9 @@ package darkRealm;
 
 public class BST_To_GreaterTree {
 
-//  Given a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus sum of all keys greater than the original key in BST.
+//  538. Convert BST to Greater Tree
+//  Given a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the 
+//  original key plus sum of all keys greater than the original key in BST.
 //
 //  Example:
 //
