901453c05f8587f23c3b00aad30d287e78f37962
Jayam Malviya <malviya.jayam@gmail.com>
2020-01-19 15:46:10 -0800

    Update MinimumMovesToEqualArrayElements.java

--- src/darkRealm/MinimumMovesToEqualArrayElements.java
+++ src/darkRealm/MinimumMovesToEqualArrayElements.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class MinimumMovesToEqualArrayElements {
 
+//  453. Minimum Moves to Equal Array Elements
 //  Given a non-empty integer array of size n, find the minimum number of moves required to make all array elements
 //  equal, where a move is incrementing n - 1 elements by 1.
 //  Example:
