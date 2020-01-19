f476e8beb374ebdeb695f519f6d7c8270e49b2f9
Jayam Malviya <malviya.jayam@gmail.com>
2020-01-19 15:01:45 -0800

    Update MajorityElement.java

--- src/darkRealm/MajorityElement.java
+++ src/darkRealm/MajorityElement.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 public class MajorityElement {
 
+//  169. Majority Element
 //  Given an array of size n, find the majority element. The majority element is the element that appears more than
 // ⌊ n/2 ⌋ times.
 //  You may assume that the array is non-empty and the majority element always exist in the array.
