c023f6ad95dbf2d2dac74b07f0ceaca98bf007af
Jayam Malviya <malviya.jayam@gmail.com>
2020-01-19 13:50:35 -0800

    Update RansomNote.java

--- src/darkRealm/RansomNote.java
+++ src/darkRealm/RansomNote.java
@@ -3,7 +3,10 @@ package darkRealm;
 import java.util.HashMap;
 
 public class RansomNote {
-//  Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+  
+//  383. Ransom Note
+//  Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will 
+//  return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
 //  Each letter in the magazine string can only be used once in your ransom note.
 //  You may assume that both strings contain only lowercase letters.
 //  canConstruct("a", "b") -> false
@@ -11,17 +14,16 @@ public class RansomNote {
 //  canConstruct("aa", "aab") -> true
 
   public static boolean canConstruct(String ransomNote, String magazine) {
-    HashMap<Character, Integer> map = new HashMap<>();
-    for (char c : ransomNote.toCharArray())
-      map.put(c, map.getOrDefault(c, 0) + 1);
-
-    for (char c : magazine.toCharArray())
-      if (map.containsKey(c))
-        if (map.get(c) == 1)
-          map.remove(c);
-        else
-          map.put(c, map.get(c) - 1);
-    return map.size() == 0;
+    int [] map = new int[256];
+    for(char c : magazine.toCharArray())
+      map[c]++;
+    
+    for(char c : ransomNote.toCharArray()){
+      if(map[c] == 0) return false;
+      map[c]--;
+    }
+    
+    return true;
   }
 
   public static void main(String[] args) {
