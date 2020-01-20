7cd5eab61e8a090e3ea1a55a939c66d585246830
midNight-jam <malviya.jayam@gmail.com>
2020-01-19 20:24:48 -0800

    SymmetricTree.java

--- src/darkRealm/SymmetricTree.java
+++ src/darkRealm/SymmetricTree.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class SymmetricTree {
 
+//  101. Symmetric Tree
 //  Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
 //
 //  For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
@@ -33,21 +34,44 @@ public class SymmetricTree {
     }
   }
 
+  public static boolean isSymmetric(TreeNode root) {
+    if (root == null) return true;
+    List<TreeNode> que = new LinkedList<>(); // Use List instead of Que as we can have get(i) via list interface
+    int toProcess;
+    que.add(root);
+    while (!que.isEmpty()) {
+
+      for (int i = 0, j = que.size() - 1; i <= j; i++, j--)
+        if (que.get(i).val != que.get(j).val) return false;
+
+      toProcess = que.size();
+      while (toProcess-- > 0) {
+        TreeNode trav = que.remove(0);
+        if (trav.val == Integer.MIN_VALUE) continue; // skip all the nulls
+        que.add(trav.left != null ? trav.left : new TreeNode(Integer.MIN_VALUE)); // treat MIN_VALUE as nulls
+        que.add(trav.right != null ? trav.right : new TreeNode(Integer.MIN_VALUE));
+      }
+    }
+
+    return true;
+  }
+
+
   public static boolean levelOrder(TreeNode trav) {
     List<TreeNode> queue = new LinkedList<>();
     queue.add(trav);
     int size = queue.size();
     while (queue.size() != 0) {
       size = queue.size();
-      for(int i = 0, j = size - 1; i <= j; i++, j--)
-          if(queue.get(i).val != queue.get(j).val) return false;
-      while(size!=0){
+      for (int i = 0, j = size - 1; i <= j; i++, j--)
+        if (queue.get(i).val != queue.get(j).val) return false;
+      while (size != 0) {
         TreeNode processed = queue.remove(0);
         size--;
-        if(processed.val == Integer.MIN_VALUE) continue;
-        if(processed.left == null) queue.add(new TreeNode(Integer.MIN_VALUE));
+        if (processed.val == Integer.MIN_VALUE) continue;
+        if (processed.left == null) queue.add(new TreeNode(Integer.MIN_VALUE));
         else queue.add(processed.left);
-        if(processed.right == null) queue.add(new TreeNode(Integer.MIN_VALUE));
+        if (processed.right == null) queue.add(new TreeNode(Integer.MIN_VALUE));
         else queue.add(processed.right);
       }
     }
@@ -84,6 +108,9 @@ public class SymmetricTree {
 //    root.left.left = new TreeNode(3);
 //
     boolean res = isSymmetricTreeLevel(root);
+    System.out.println("IsSymmetric : " + res);
+
+    res = isSymmetricroot);
     System.out.println("IsSymmetric : " + res);
   }
 }
