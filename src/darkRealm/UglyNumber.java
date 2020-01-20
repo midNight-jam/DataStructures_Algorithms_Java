73bae218d3489f6f5cbf0ff85ab328c1e72b4f12
midNight-jam <malviya.jayam@gmail.com>
2020-01-19 20:03:09 -0800

    UglyNumber.java

--- src/darkRealm/UglyNumber.java
+++ src/darkRealm/UglyNumber.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 public class UglyNumber {
 
+//  263. Ugly Number
 //  Write a program to check whether a given number is an ugly number.
 //  Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. For example, 6, 8 are ugly while 14 is
 //  not ugly since it includes another prime factor 7.
@@ -13,10 +14,20 @@ public class UglyNumber {
 //  Credits:
 //  Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
 
+  public static boolean isUglyBasic(int num) {
+    if(num == 0) return false;
+    int [] primes =  new int[]{2, 3, 5};
+    for(int p : primes){
+      while(num % p == 0)
+        num = num / p;
+    }
+
+    return num == 1;
+  }
+
   public static boolean uglyNumber(int n) {
     Set<Integer> primes = new HashSet<>(Arrays.asList(new Integer[]{1, 2, 3, 5}));
     int factor = 2;
-    Set<Integer> factors = new HashSet<>();
     for (; factor * factor <= n; factor++) {
       while (n % factor == 0)
         n = n / factor;
