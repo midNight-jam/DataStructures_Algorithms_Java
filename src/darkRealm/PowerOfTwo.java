07aa786fb7298abda5d33e03131853c56970a2f1
midNight-jam <malviya.jayam@gmail.com>
2020-01-24 22:05:01 -0800

    PowerOfTwo.java

--- src/darkRealm/PowerOfTwo.java
+++ src/darkRealm/PowerOfTwo.java
@@ -1,6 +1,25 @@
 package darkRealm;
 
 public class PowerOfTwo {
+
+//  231. Power of Two
+//  Given an integer, write a function to determine if it is a power of two.
+//
+//  Example 1:
+//
+//  Input: 1
+//  Output: true
+//  Explanation: 20 = 1
+//  Example 2:
+//
+//  Input: 16
+//  Output: true
+//  Explanation: 24 = 16
+//  Example 3:
+//
+//  Input: 218
+//  Output: false
+
   public static boolean powerOfTwo(int n) {
     if (1 > n) return false;
     int n_1 = n - 1;
