4c69175f03c8146d882c398b6cc964427244121f
Jayam Malviya <malviya.jayam@gmail.com>
2020-01-25 00:07:48 -0800

    Update IntersectionArrays.java

--- src/darkRealm/IntersectionArrays.java
+++ src/darkRealm/IntersectionArrays.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class IntersectionArrays {
 
+//  350. Intersection of Two Arrays II
 //  Given two arrays, write a function to compute their intersection.
 //  Example:
 //  Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2, 2].
