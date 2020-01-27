9fdf8e1e2ff2737ed8f2568c50e91d4270c90afb
midNight-jam <malviya.jayam@gmail.com>
2020-01-26 17:24:17 -0800

    ReverseOfVowels.java

--- src/darkRealm/ReverseOfVowels.java
+++ src/darkRealm/ReverseOfVowels.java
@@ -1,7 +1,10 @@
 package darkRealm;
 
+import java.util.Stack;
+
 public class ReverseOfVowels {
 
+//  345. Reverse Vowels of a String
 //  Write a function that takes a string as input and reverse only the vowels of a string.
 //      Example 1:
 //  Given s = "hello", return "holle".
@@ -14,10 +17,10 @@ public class ReverseOfVowels {
     if (null == s || 0 == s.length()) return s;
     char[] sarr = s.toCharArray();
     int i = 0, j = sarr.length - 1;
-    while (i <= j) {
+    while (i < j) {
       while (i < sarr.length && !isVowel(sarr[i])) i++;
       while (j > -1 && !isVowel(sarr[j])) j--;
-      if (i <= j) {
+      if (i < j) {
         char t = sarr[i];
         sarr[i++] = sarr[j];
         sarr[j--] = t;
@@ -29,6 +32,30 @@ public class ReverseOfVowels {
   public static boolean isVowel(char c) {
     return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
         || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+  }
+
+
+  public static String reverseVowelsBasic(String s) {
+    if (s == null || s.length() < 1) return s;
+    Stack<Character> stack = new Stack<>();
+    char c;
+    for (int i = 0; i < s.length(); i++) {
+      c = s.charAt(i);
+      if (!isVowel(c))
+        continue;
+      stack.push(c);
+    }
+
+    char[] arr = s.toCharArray();
+    for (int i = 0; i < arr.length; i++) {
+      c = s.charAt(i);
+      if (!isVowel(c))
+        arr[i] = c;
+      else
+        arr[i] = stack.pop();
+    }
+
+    return new String(arr);
   }
 
   public static void main(String[] args) {
