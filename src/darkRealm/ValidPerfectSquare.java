6351fa597a7cbccb296199780c731a97fe61c6bb
Jayam Malviya <malviya.jayam@gmail.com>
2020-01-26 17:53:13 -0800

    Update ValidPerfectSquare.java

--- src/darkRealm/ValidPerfectSquare.java
+++ src/darkRealm/ValidPerfectSquare.java
@@ -2,6 +2,7 @@ package darkRealm;
 
 public class ValidPerfectSquare {
 
+//  367. Valid Perfect Square
 //  Given a positive integer num, write a function which returns True if num is a perfect square else False.
 //  Note: Do not use any built-in library function such as sqrt.
 //  Example 1:
