b74137899f59de9666307a9a4227451662ae5715
midNight-jam <malviya.jayam@gmail.com>
2020-01-26 19:00:39 -0800

    ArrangeCoins.java

--- src/darkRealm/ArrangeCoins.java
+++ src/darkRealm/ArrangeCoins.java
@@ -2,6 +2,7 @@ package darkRealm;
 
 public class ArrangeCoins {
 
+//  441. Arranging Coins
 //  You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
 //  Given n, find the total number of full staircase rows that can be formed.
 //  n is a non-negative integer and fits within the range of a 32-bit signed integer.
@@ -21,6 +22,33 @@ public class ArrangeCoins {
 //  ¤ ¤
 //  Because the 4th row is incomplete, we return 3
 
+
+  public int arrangeCoinsLogN(int n) {
+    if (n == 1) return 1;
+    // sum of first N numbers  = n(n+1)/2;
+    long left = 1; // long for overflows
+    long right = n;
+    long mid;
+    long sum;
+
+    while (left < right) {
+      // would represent the mid or Mth row, if sum upto M rows is equal to mid, then we have M complete rows,
+      // else we shift the left & right to reach the mid / Mth row.
+      mid = left + (right - left) / 2;
+      sum = (mid * (mid + 1) / 2);
+      if (sum < n)
+        left = mid + 1;
+      else if (sum > n)
+        right = mid;
+
+      // else sum is completely matching, then Mth/ mid row is the last complete row
+      else return (int) mid;
+    }
+
+    // if left >= right means the last row  return the prev to left row;
+    return (int) (left - 1);
+  }
+
   public static int arrangeCoins(int n) {
     int start = 0;
     int end = n;
@@ -36,7 +64,7 @@ public class ArrangeCoins {
     return start - 1;
   }
 
-  public static int arrangeCoinsOLD(int n) {
+  public static int arrangeCoins_O_of_N(int n) {
     if (1 > n) return 0;
     int i = 0;
     for (; n > 0; i++) {
