97082318bfa9f62ef49ca616157f4354f3496751
Jayam Malviya <malviya.jayam@gmail.com>
2020-01-26 17:06:27 -0800

    Update PowerOfFour.java

--- src/darkRealm/PowerOfFour.java
+++ src/darkRealm/PowerOfFour.java
@@ -2,6 +2,7 @@ package darkRealm;
 
 public class PowerOfFour {
 
+//  342. Power of Four
 //  Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
 //  Example:
 //  Given num = 16, return true. Given num = 5, return false.
