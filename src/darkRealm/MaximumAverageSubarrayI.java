cf339a8eb695abe4a4e2358c6d260d8c4d2ae282
midNight-jam <malviya.jayam@gmail.com>
2020-01-30 23:10:40 -0800

    MaximumAverageSubarrayI.java

--- src/darkRealm/MaximumAverageSubarrayI.java
+++ src/darkRealm/MaximumAverageSubarrayI.java
@@ -3,7 +3,8 @@ package darkRealm;
 import java.util.Arrays;
 
 public class MaximumAverageSubarrayI {
-// #643 -  Maximum Average Subarray I
+
+//  643 -  Maximum Average Subarray I
 //  Given an array consisting of n integers, find the contiguous subarray of given length k that has the maximum
 //  average value. And you need to output the maximum average value.
 //      Example 1:
@@ -16,28 +17,31 @@ public class MaximumAverageSubarrayI {
 
 
   public static double findMaxAverage(int[] nums, int k) {
-    if (k > nums.length) return Double.MIN_VALUE;
-    double maxAverage = Double.MIN_VALUE, sum = 0.0;
-    int start = 0, end = 0;
-    while (end < k) {
-      sum += nums[end];
-      end++;
-    }
-    maxAverage = sum / k;
+    if (nums == null || k <= 0 || k > nums.length) return 0.0;
+    double sum = 0;
+    double avg = 0.0;
 
-    while (end < nums.length) {
-      sum -= nums[start];
-      start++;
-      sum += nums[end];
-      end++;
-      maxAverage = Math.max(maxAverage, sum / k);
+    for (int i = 0; i < k; i++)
+      sum += nums[i];
+
+    avg = sum / k;
+    double max = avg;
+
+    for (int i = k; i < nums.length; i++) {
+      sum -= nums[i - k];
+      sum += nums[i];
+      avg = sum / k;
+      max = Math.max(avg, max);
     }
-    return maxAverage;
+    return max;
   }
 
+
   public static void main(String[] args) {
-    int[] nums = new int[]{1, 12, -5, -6, 50, 3};
-    int k = 4;
+//    int[] nums = new int[]{1, 12, -5, -6, 50, 3};
+//    int k = 4;
+    int[] nums = new int[]{-1};
+    int k = 1;
     double res = findMaxAverage(nums, k);
     System.out.println("N : " + Arrays.toString(nums) + "\nK: " + k + "\nR: " + res);
   }
