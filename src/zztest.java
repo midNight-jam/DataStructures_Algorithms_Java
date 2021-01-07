5567b64bab1282f7cfb4c4754362e129375494bc
Jayam Malviya <jamalviy@microsoft.com>
2021-01-07 08:55:11 -0800

    fromWOA

--- src/zztest.java
+++ src/zztest.java
@@ -2,6 +2,6 @@ public class zztest {
 
     public static void main(String[] args) {
         System.out.println("@>@ Test here...");
-        System.out.println("@>@ ");
+        System.out.println("@>@  this should get commited");
     }
 }
